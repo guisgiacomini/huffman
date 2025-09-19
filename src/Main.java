4453faaee5c3c1b0168f5eac8326a4e6ff138001
CarlosDinizz <carlosdzalmeida@gmail.com>
2025-09-18 21:41:53 -0300

    Lendo arquivos feito.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello, world!");
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0 || args.length > 2){
+            throw new RuntimeException("É necessário 2 argumentos. Argumentos recebidos: " + args.length);
+        }
+
+        String arquivoOriginal = args[0];
+        String arquivoComprimido = args[1];
+
+        String texto = "";
+        String linha = "";
+        BufferedReader reader = new BufferedReader(new FileReader(arquivoOriginal));
+
+
+        linha = reader.readLine();
+        while (linha != null){
+            texto = texto + "" + linha;
+            linha = reader.readLine();
+        }
+        reader.close();
+
+        System.out.println(texto);
     }
 }
